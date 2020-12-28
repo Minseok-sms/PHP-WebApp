b107eb843b15658013c4078d2956bc51ebfcef52
Minseok-sms <tjalstjr111@gmail.com>
2020-12-29 01:00:43 +0900

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+wbtn = document.getElementById('white_btn');
+wbtn.addEventListener('click',function(){document.getElementById('change').className = 'white';})
+
+bbtn = document.getElementById('black_btn');
+bbtn.addEventListener('click',function(){document.getElementById('change').className = 'black';})
